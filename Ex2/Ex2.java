1bdf2195b02c574f0aab6e16963923adcedb705f
zhangmingsheng <z1m6s3@163.com>
2016-12-01 15:56:08 +0800

    Ex2

---
+++ Ex2/Ex2.java
@@ -0,0 +1,60 @@
+package conf_1130;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.mockito.exceptions.Reporter;
+
+public class Ex2 {
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+		private HashSet<String> ip_time = new HashSet<String>();
+		private String ip_num = null;
+
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String[] line = value.toString().split("\t");
+			ip_time.add(line[0]);
+			ip_num = String.valueOf(ip_time.size());
+		}
+
+		public void cleanup(Context context) throws IOException,
+				InterruptedException {
+			context.write(new Text("IP number"), new Text(ip_num));
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
+		public void reduce(Text key, Iterator<Text> values,
+				OutputCollector<Text, Text> output, Reporter report)
+				throws IOException {
+		}
+	}
+
+	public static void main(String[] args) throws IOException,
+			ClassNotFoundException, InterruptedException {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf);
+		job.setJarByClass(conf_setup_1130.class);
+		job.setMapperClass(Map.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setReducerClass(Reduce.class);
+		job.setNumReduceTasks(0);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+		return;
+	}
+}
