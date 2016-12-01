5ba1d7ff12c7fec7e9110b52239d4b41279f9fb7
Chen Weican <chenweican@gmail.com>
2016-12-01 10:01:09 +0800

    Create ex1.java

---
+++ Ex1/ex1.java
@@ -0,0 +1,42 @@
+public class QSTLinkList 
+{
+	public static class Node{
+		public int value;
+		public Node next = null;
+		Node(int value){
+			this.value = value;
+		}
+		Node(int value, Node next){
+			this.value = value;
+			this.next = next;
+		}
+		public void setValue(int value){
+			this.value = value;
+		}
+	}
+	private static void printLinkList(Node head) {
+	}
+  
+  public static void main( String[] args ){
+    int[] arr = {1,3,5,7,2,4};
+    Node head = createLinkList(arr);
+    printLinkList(head);
+  }
+	private static Node createLinkList(int[] arr) {
+		// TODO Auto-generated method stub
+		Node[] linkArr = new Node[arr.length];
+		for (int i=0; i<arr.length; i++){
+			linkArr[i] = new Node(arr[i]);
+		}
+		for (int i=0; i<arr.length; i++){
+			linkArr[i].setValue(arr[i]);
+			if (i == arr.length - 1){
+				linkArr[i].next = null;
+			}
+			else{
+				linkArr[i].next = linkArr[i+1];
+			}
+		}
+		return linkArr[0];
+	}
+}
