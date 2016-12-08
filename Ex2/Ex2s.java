e0915edcc395ac7b5afac22c79f6a950ebf1fd46
zhangmingsheng <z1m6s3@163.com>
2016-12-08 08:54:20 +0800

    Add files via upload

---
+++ Ex2/Ex2s.java
@@ -0,0 +1,58 @@
+package conf_1130;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Ex2s {
+
+	public static class Map extends Mapper<Object, Text, Text, NullWritable> {
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			String[] line = value.toString().split("\t");
+			context.write(new Text(line[0]), NullWritable.get());
+		}
+	}
+
+	public static class Reduce extends
+			Reducer<Text, NullWritable, Text, LongWritable> {
+		long count = 0;
+
+		public void reduce(Text key, Iterable<NullWritable> values,
+				Context context) throws IOException, InterruptedException {
+			count++;
+		}
+
+		public void cleanup(
+				Reducer<Text, NullWritable, Text, LongWritable>.Context context)
+				throws IOException, InterruptedException {
+			super.cleanup(context);
+			context.write(new Text("IP Deduplication"),
+							new LongWritable(count));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		Job job = Job.getInstance(conf, "IpDeduplication");
+		job.setJarByClass(Ex2s.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		job.setMapOutputValueClass(NullWritable.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LongWritable.class);
+		job.setNumReduceTasks(1);
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.waitForCompletion(true);
+		return;
+	}
+}
